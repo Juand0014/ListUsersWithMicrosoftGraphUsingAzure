34322004eeae6170bbbca2b86e3761158ff50452
Juand0014 <juand0014@hotmail.com>
2021-08-11 12:41:08 -0400

    add backend

---
+++ Server/ApiGraph/Controllers/GraphController.cs
@@ -0,0 +1,23 @@
+﻿using ApiGraph.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGraph.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GraphController : Controller
+    {
+        public GraphServices graphService = new GraphServices();
+
+        [HttpGet]
+        public async Task<IActionResult> GetTokenAsync()
+        {
+            var result = await graphService.GetAllUsers();
+            return Ok(result);
+        }
+    }
+}
---
+++ Server/ApiGraph/Services/GraphServices.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGraph.Services
+{
+    public class GraphServices
+    {
+        public static string clientId = "bdaba269-9551-4bb9-843e-6ec0d528a1d9";
+        public static string clientSecret = "-Xor.lc6_i0Zm82hubyK_~7po_p4ooFDlA";
+        public static string tenantId = "58a9ad3d-abd0-479b-adf1-2591bd993068";
+
+        public async Task<IGraphServiceUsersCollectionPage> GetAllUsers()
+        {
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var graphClient = new GraphServiceClient(clientSecretCredential);
+            var users = await graphClient.Users.Request().GetAsync();
+            return users;
+        }
+    }
+}
